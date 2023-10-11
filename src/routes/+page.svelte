349773ccaaae91415313cb758aff7996a0115228
Javier Bergasa Palacios <64484123+jagitdev@users.noreply.github.com>
2023-10-11 13:44:18 +0200

    primer commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="h1">Welcome to SvelteKit</h1>
+<p>
+  Visits <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style lang="scss">
+  h1 {
+    color: red;
+  }
+</style>
